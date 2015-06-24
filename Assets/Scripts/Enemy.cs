71533988ad9e6ff18fc65894664109a9f6363448
Ronnie E <rel-alam@hotmail.com>
2015-06-24 14:58:14 +1000

    + new stuff

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -23,25 +23,25 @@ public class Enemy : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        GameObject castle = GameObject.Find("Castle");
-        GameObject tower = GameObject.Find("Tower");
-       // if (tower != null)
-       // {
-       //     distance = Vector3.Distance(castle.transform.position, tower.transform.position);
-       // }
-       // else
-       // {
-       //     distance = 0;
-       // }
-	   //
-       // if (distance < 10)
-       // {
-       //     GetComponent<NavMeshAgent>().destination = castle.transform.position;
-       // }
-       // else
-       // {
-       //     GetComponent<NavMeshAgent>().destination = tower.transform.position;
-       // }
+        //GameObject castle = GameObject.Find("Castle");
+        //GameObject tower = GameObject.Find("Tower");
+        //if (tower != null)
+        //{
+        //    distance = Vector3.Distance(castle.transform.position, tower.transform.position);
+        //}
+        //else
+        //{
+        //    distance = 0;
+        //}
+	    //
+        //if (distance < 10)
+        //{
+        //    GetComponent<NavMeshAgent>().destination = castle.transform.position;
+        //}
+        //else
+        //{
+        //    GetComponent<NavMeshAgent>().destination = tower.transform.position;
+        //}
 	}
 
 	void OnTriggerEnter(Collider co)
